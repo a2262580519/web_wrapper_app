6bd9a6d4b3717c3e07f1f124d2cbf29473709d88
a2262580519 <a2262580519@users.noreply.github.com>
2026-09-20 17:29:30 +0800

    feat: init web wrapper app for freqtrade ui

---
+++ android/app/src/main/kotlin/com/example/web_wrapper_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_wrapper_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // 设置状态栏透明与深色图标，达到沉浸式最大化显示效果
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Freqtrade UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('WebResourceError: ${error.description}');
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('http://47.236.198.163:8081/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, dynamic result) async {
+        if (didPop) return;
+        // 如果网页内部有前进后退历史，优先后退
+        if (await _controller.canGoBack()) {
+          await _controller.goBack();
+        } else {
+          // 退到首页后再按返回键退出 APP
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        // 去掉默认 AppBar，全屏最大化显示
+        body: SafeArea(
+          top: false, // 顶部不占位，最大化全屏展示
+          bottom: true,
+          child: Stack(
+            children: [
+              WebViewWidget(controller: _controller),
+              if (_isLoading)
+                const Center(
+                  child: CircularProgressIndicator(),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
